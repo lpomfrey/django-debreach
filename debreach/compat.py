68d94c9418adba188ecb3a85a8b1c3496d95393c
Luke Pomfrey <luke@lukepomfrey.org>
2013-08-10 14:15:26 +0100

    I can't spell.

--- debreach/compat.py
+++ debreach/compat.py
@@ -6,7 +6,7 @@ try:
     # Django >= 1.4.5
     from django.utils.encoding import force_bytes, force_text, smart_text  # NOQA
     from django.utils.six import string_types, text_type  # NOQA
-except ImportError:  # pragme: no cover
+except ImportError:  # pragma: no cover
     # Django < 1.4.5
     from django.utils.encoding import (                         # NOQA
         smart_unicode as smart_text, smart_str as force_bytes)  # NOQA
@@ -17,7 +17,7 @@ except ImportError:  # pragme: no cover
 try:
     # Django >= 1.4
     from django.utils.crypto import get_random_string  # NOQA
-except ImportError:  # pragme: no cover
+except ImportError:  # pragma: no cover
     # Django < 1.4
     from random import choice
     get_random_string = lambda n: ''.join(
