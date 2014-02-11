d3efef19674274fd9c19437bb6a48fdecad0442a
Luke Pomfrey <luke@lukepomfrey.org>
2014-02-11 20:28:25 +0000

    Add AppConfig for Django 1.7

--- debreach/__init__.py
+++ debreach/__init__.py
@@ -6,3 +6,5 @@ from distutils import version
 
 __version__ = '0.9.0'
 version_info = version.StrictVersion(__version__).version
+
+default_app_config = 'debreach.apps.DebreachConfig'
---
+++ debreach/apps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+try:
+    from django.apps import AppConfig
+except ImportError:
+    class AppConfig(object):
+        pass
+
+
+class DebreachConfig(AppConfig):
+
+    name = 'debreach'
