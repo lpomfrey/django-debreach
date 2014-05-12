783dbdae96193d959af2fbe9a8d7362000e2d4e2
Luke Pomfrey <luke@lukepomfrey.org>
2014-05-12 20:01:49 +0100

    Bump version

--- debreach/__init__.py
+++ debreach/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from distutils import version
 
 
-__version__ = '0.9.0'
+__version__ = '1.0.0'
 version_info = version.StrictVersion(__version__).version
 
 default_app_config = 'debreach.apps.DebreachConfig'
