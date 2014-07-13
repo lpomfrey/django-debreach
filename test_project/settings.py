bdb258162e610a14ef1a513313c502520075ffc6
hwkns <danieldhawkins@gmail.com>
2014-07-12 21:37:02 -0400

    added TEST_RUNNER to settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -8,6 +8,8 @@ DIRNAME = os.path.dirname(__file__)
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
