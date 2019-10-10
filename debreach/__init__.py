263fb11f28cefe97c3250fde2536c268e83a8017
Luke Pomfrey <luke@lukepomfrey.org>
2019-10-10 10:09:05 +0100

    Bump version to 2.0.1

--- debreach/__init__.py
+++ debreach/__init__.py
@@ -2,7 +2,7 @@
 from distutils import version
 
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 version_info = version.StrictVersion(__version__).version
 
 default_app_config = 'debreach.apps.DebreachConfig'
